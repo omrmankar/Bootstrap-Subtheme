0775b8009fa3779f0e05dbb0da15472e0610f2b4
devendra <webexpertz.ntf@gmail.com>
2018-07-09 11:19:23 +0530

    change file

---
+++ text.php
@@ -0,0 +1,61 @@
+git clone
+git init
+git add -A
+git commit -m "add all theme file"
+git push -u origin master
+ 
+git config user.email omrmankar@gmail.com
+ 
+git config user.name omrmankar
+git clone
+git init
+git add -A
+git commit -m "add all theme file"
+git push -u origin master
+ 
+git config user.email omrmankar@gmail.com
+ 
+git config user.name omrmankar
+
+
+git clone
+git init
+git add -A
+git commit -m "add all theme file"
+git push -u origin master
+ 
+git config user.email omrmankar@gmail.com
+ 
+git config user.name omrmankar
+
+
+git clone
+git init
+git add -A
+git commit -m "add all theme file"
+git push -u origin master
+ 
+git config user.email omrmankar@gmail.com
+ 
+git config user.name omrmankar
+
+
+git clone
+git init
+git add -A
+git commit -m "add all theme file"
+git push -u origin master
+ 
+git config user.email omrmankar@gmail.com
+ 
+git config user.name omrmankar
+
+git clone
+git init
+git add -A
+git commit -m "add all theme file"
+git push -u origin master
+ 
+git config user.email omrmankar@gmail.com
+ 
+git config user.name omrmankar
