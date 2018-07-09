0a140f21e12f91e6d7548d861bc49e77cf7c8237
devendra <webexpertz.ntf@gmail.com>
2018-07-09 12:54:40 +0530

    change file sss

--- text.php
+++ text.php
@@ -1,61 +1,7 @@
-git clone
-git init
-git add -A
-git commit -m "add all theme file"
-git push -u origin master
- 
-git config user.email omrmankar@gmail.com
- 
-git config user.name omrmankar
-git clone
-git init
-git add -A
-git commit -m "add all theme file"
-git push -u origin master
- 
-git config user.email omrmankar@gmail.com
- 
-git config user.name omrmankar
+test vinay
+test vinay
+test vinay
 
-
-git clone
-git init
-git add -A
-git commit -m "add all theme file"
-git push -u origin master
- 
-git config user.email omrmankar@gmail.com
- 
-git config user.name omrmankar
-
-
-git clone
-git init
-git add -A
-git commit -m "add all theme file"
-git push -u origin master
- 
-git config user.email omrmankar@gmail.com
- 
-git config user.name omrmankar
-
-
-git clone
-git init
-git add -A
-git commit -m "add all theme file"
-git push -u origin master
- 
-git config user.email omrmankar@gmail.com
- 
-git config user.name omrmankar
-
-git clone
-git init
-git add -A
-git commit -m "add all theme file"
-git push -u origin master
- 
-git config user.email omrmankar@gmail.com
- 
-git config user.name omrmankar
+test vinay
+test vinay
+test vinay
